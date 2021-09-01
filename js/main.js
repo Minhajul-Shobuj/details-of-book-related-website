482f9ac05ff02559e8907d48bfbd7e681af9eddd
Minhajul-Shobuj <mdminhajulislamshobuj@gmail.com>
2021-09-02 04:01:46 +0600

    update

--- js/main.js
+++ js/main.js
@@ -8,19 +8,27 @@ const searchText = () => {
 }
 
 const displayResult = (data) => {
-    const results = document.getElementById('results');
-    results.innerHTML = '';
-    // for book value
-    const resultValue = document.getElementById('result-value');
-    resultValue.innerHTML = '';
-    const h5 = document.createElement('h5');
-    h5.innerHTML = `Showing <span class="text-success fw-bolder">${data.length}</span> books`;
-    resultValue.appendChild(h5);
-    // for book with details 
-    data.forEach(element => {
-        const div = document.createElement('div');
-        div.classList.add('col')
-        div.innerHTML = `<div class="d-flex flex-row card">
+    if (data.length === 0) {
+        //for no result
+        const noResult = document.getElementById('no-result');
+        const h1 = document.createElement('h1')
+        h1.innerHTML = `<h1>"No Result Found{--}"</h1>`
+        noResult.appendChild(h1);
+    }
+    else {
+        const results = document.getElementById('results');
+        results.innerHTML = '';
+        // for book value
+        const resultValue = document.getElementById('result-value');
+        resultValue.innerHTML = '';
+        const h5 = document.createElement('h5');
+        h5.innerHTML = `Showing <span class="text-success fw-bolder">${data.length}</span> books`;
+        resultValue.appendChild(h5);
+        // for book with details 
+        data.forEach(element => {
+            const div = document.createElement('div');
+            div.classList.add('col')
+            div.innerHTML = `<div class="d-flex flex-row card">
         <div>
         <img src=" https://covers.openlibrary.org/b/id/${element.cover_i}-M.jpg" class="card-img-top" alt="...">
     </div>
@@ -30,7 +38,8 @@ const displayResult = (data) => {
             <p>First published in ${element.first_publish_year}</p>
         </div>
     </div>`
-        results.appendChild(div);
-    });
+            results.appendChild(div);
+        });
+    }
 
 }
